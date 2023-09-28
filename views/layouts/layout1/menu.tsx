339f3db3eb04cac01f69bdb119ddbde1713b1026
Muhammad Kashif <muhammadkashifbhatti70@gmail.com>
2023-09-28 19:59:34 +0500

    mzubair code removed

--- views/layouts/layout1/menu.tsx
+++ views/layouts/layout1/menu.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from "react";
-import Dropdown from "react-bootstrap/Dropdown";
-import Carousel from "react-bootstrap/Carousel";
-import { Media } from "reactstrap";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Carousel from 'react-bootstrap/Carousel';
+import { Media } from 'reactstrap';
+import { useRouter } from 'next/router';
+
 
 type MenuProps = {
   meneData: any;
@@ -12,7 +14,7 @@ const Menu = ({ meneData }: MenuProps) => {
   const router = useRouter();
   const [menuData, setMenuData] = useState([]);
   const [openMenuIndex, setOpenMenuIndex] = useState(null);
-  const [banners, setBanners] = useState([]);
+  const [banners , setBanners] = useState([])
 
   const handleMouseEnter = (index) => {
     setOpenMenuIndex(index);
@@ -23,13 +25,14 @@ const Menu = ({ meneData }: MenuProps) => {
   };
 
   function transformImageUrl(apiImageUrl) {
-    return `http://18.235.14.45${apiImageUrl.replace(/ /g, "%20")}`;
+    return `http://18.235.14.45${apiImageUrl.replace(/ /g, '%20')}`;
   }
 
   const handleClick = (id) => (e) => {
     e.preventDefault();
     window.location.href = `/collections/leftsidebar?sub_category=${id}`;
   };
+  
 
   useEffect(() => {
     setMenuData(meneData.menus);
@@ -37,56 +40,50 @@ const Menu = ({ meneData }: MenuProps) => {
   }, []);
 
   return (
-    <div className="container">
-      <div className="row pb-4 pb-md-0">
-        <div className="col-md-4 pb-4">
+    <div className="custom-container">
+      <div className='d-flex gap-3'>
+        <div className=''>
           <div className="custom-menus mb-3" onMouseLeave={handleMouseLeave}>
             <div className="custom-input-group-text">
               <h4>Our Menu & Categories</h4>
             </div>
             {Object.keys(menuData).map((menuKey, index) => (
-              <Dropdown
+              <DropdownButton
                 key={index}
                 drop="end"
-                show={openMenuIndex === index}
-                onMouseEnter={() => handleMouseEnter(index)}
+                className='custom-dropdown-menu'
+                show={openMenuIndex === index} // Control the visibility of the dropdown menu for this menu item
+                title={
+                  <div
+                    className="custom-text-black"
+                    onMouseEnter={() => handleMouseEnter(index)} // Set the index when mouse enters
+                  >
+                    <div>
+                      {menuData[menuKey].menu_name.substring(0, 23)}
+                      {menuData[menuKey].menu_name.length > 25 ? '...' : ''}
+                    </div>
+                  </div>
+                }
               >
-                <Dropdown.Toggle
-                  id={`menu-dropdown-${index}`}
-                  className="custom-toggle-button"
-                >
-                  {menuData[menuKey].menu_name}
-                </Dropdown.Toggle>
-                <Dropdown.Menu className="side-menu-box">
-                  {menuData[menuKey].categories?.map(
-                    (category, categoryIndex) => (
-                      <div
-                        className="custom-side-menu-category"
-                        key={categoryIndex}
-                      >
-                        <Dropdown.Item href={category.link}>
-                          <h5>{category.name}</h5>
+                <div className='side-menu-box'>
+                  {menuData[menuKey].categories?.map((category, categoryIndex) => (
+                    <div className="custom-side-menu-category">
+                      <Dropdown.Item key={categoryIndex} href={category.link}>
+                        <h5>{category.name}</h5>
+                      </Dropdown.Item>
+                      {category.sub_categories?.map((subcategory, subcategoryIndex) => (
+                        <Dropdown.Item key={subcategoryIndex} href={subcategory.link} onClick={handleClick(subcategory.id)}>
+                          {subcategory.name}
                         </Dropdown.Item>
-                        {category.sub_categories?.map(
-                          (subcategory, subcategoryIndex) => (
-                            <Dropdown.Item
-                              key={subcategoryIndex}
-                              href={subcategory.link}
-                              onClick={handleClick(subcategory.id)}
-                            >
-                              {subcategory.name}
-                            </Dropdown.Item>
-                          )
-                        )}
-                      </div>
-                    )
-                  )}
-                </Dropdown.Menu>
-              </Dropdown>
+                      ))}
+                    </div>
+                  ))}
+                </div>
+              </DropdownButton>
             ))}
           </div>
         </div>
-        <div className="col-md-6">
+        <div className='slider-top w-50'>
           <Carousel>
             {banners.map((banner, index) => (
               <Carousel.Item key={index}>
@@ -99,10 +96,10 @@ const Menu = ({ meneData }: MenuProps) => {
             ))}
           </Carousel>
         </div>
-        <div className="col-md-2">
+        <div className='menu-banners'>
           <Media
-            className="d-block w-100 b-img h-100"
-            src="/images/layout-2/slider/nextimg.jpeg"
+            className="d-block w-100 b-img"
+            src='/images/layout-2/slider/nextimg.jpeg'
             alt="Banner 1"
           />
         </div>
